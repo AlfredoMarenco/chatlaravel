bd351582db725438c3ad1e239a4f97de241e464a
unknown <alfredomarenco@boletea.com>
2022-06-12 12:45:10 -0500

    Cambios

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -4,10 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Chat extends Model
 {
     use HasFactory;
+
+    //Mutadores
+    public function name(): Attribute
+    {
+        return new Attribute(
+            get:function($value){
+                if ($this->is_group) {
+                    return $value;
+                }
+                $user = $this->users->where('id','!=',auth()->id)->first();
+                $contact = auth()->user()->contacts()->where('contact_id',$user->id)->first();
+
+                return $contact ? $contact->name : $user->email;
+             }
+        );
+    }
+
     protected $fillable = ['name','image_url','is_group'];
 
     //Relacion muchos a muchos con tabla menssages
--- resources/views/layouts/chat.blade.php
+++ resources/views/layouts/chat.blade.php
@@ -1,40 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        @stack('css')
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        @livewireStyles
+    @stack('css')
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
+    @livewireStyles
 
-        <div class="h-32 bg-teal-600">
+    {{-- Fontawesome --}}
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
 
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+
+<body class="font-sans antialiased">
+
+    <div class="h-32 bg-teal-600">
+
+    </div>
+    <div class="absolute left-0 top-6 w-screen">
+        <div class="container mx-auto">
+            {{ $slot }}
         </div>
-        <div class="absolute left-0 top-6 w-screen">
-            <div class="container mx-auto">
-                    {{ $slot }}
-            </div>
-        </div>
+    </div>
 
-        @stack('modals')
+    @stack('modals')
 
-        @livewireScripts
+    @livewireScripts
 
-        @stack('js')
-    </body>
+    @stack('js')
+</body>
+
 </html>

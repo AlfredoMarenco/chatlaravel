b11a85999a1b406c3a7204a07c0b19a6794bd355
unknown <alfredomarenco@boletea.com>
2022-06-16 21:55:39 -0500

    Modificacion aver si sirve

--- public/js/app.js
+++ public/js/app.js
@@ -5010,7 +5010,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "5dc8e170cd1e650282fe",
+  key: window.PUSHER_APP_KEY,
   wsHost: window.location.hostname,
   wsPort: window.APP_ENV ? 6002 : 6001,
   wssPort: window.APP_ENV ? 6002 : 6001,
